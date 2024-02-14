851bbdbe7adb272b5829530aff2a8f21630be161
PrafullaS <112618564+PrafullaS@users.noreply.github.com>
2024-02-14 19:57:36 +0530

    Update variables.tf

--- Conditional/variables.tf
+++ Conditional/variables.tf
@@ -15,15 +15,3 @@ variable "development_subnet_cidr" {
   type        = string
   default     = "10.0.2.0/24"
 }
-
-resource "aws_security_group" "example" {
-  name        = "example-sg"
-  description = "Example security group"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.environment == "production" ? [var.production_subnet_cidr] : [var.development_subnet_cidr]
-  }
-}
