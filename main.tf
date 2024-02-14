3f91f10721c7f167c79d7c22671652636c69d5ef
PrafullaS <112618564+PrafullaS@users.noreply.github.com>
2024-02-14 09:37:27 +0000

    updated

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_instance" {
   ami= var.ami_value
   instance_type = var.instance_type
-  subnet_id = var.aws_subnet_id
+  # subnet_id = var.aws_subnet_id
 }
--- variables.tf
+++ variables.tf
@@ -2,9 +2,9 @@
 #     description = "value for vpc"
 # }
 
-variable "aws_subnet_id" {
-  description = "value for subnet"
-}
+# variable "aws_subnet_id" {
+#   description = "value for subnet"
+# }
 
 variable "ami_value" {
   description = "value for the ami"
