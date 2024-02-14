409b09de272287f27af34f5ce9c94ae3484fd7dd
PrafullaS <112618564+PrafullaS@users.noreply.github.com>
2024-02-14 20:04:22 +0530

    Update main.tf

--- Conditional/main.tf
+++ Conditional/main.tf
@@ -1,3 +1,8 @@
+resource "aws_instance" "my_instance" {
+  ami= var.ami_value
+  instance_type = var.instance_type
+  # subnet_id = var.aws_subnet_id
+}
 resource "aws_security_group" "example" {
   name        = "example-sg"
   description = "Example security group"
